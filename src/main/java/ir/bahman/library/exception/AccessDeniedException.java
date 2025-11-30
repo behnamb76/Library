f2f79574aaea5875cafee3e159c1ec4c1d85eee4
behnamb76 <borhanyb@gmail.com>
2025-11-30 16:08:40 +0330

    add AccessDeniedException

---
+++ src/main/java/ir/bahman/library/exception/AccessDeniedException.java
@@ -0,0 +1,7 @@
+package ir.bahman.library.exception;
+
+public class AccessDeniedException extends RuntimeException {
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+}
