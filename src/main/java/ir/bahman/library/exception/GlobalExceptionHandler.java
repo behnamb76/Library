dcba0ea308a825e8d3ef4b4b0f1cc02595d72cc8
behnamb76 <borhanyb@gmail.com>
2025-12-01 17:03:31 +0330

    add GlobalExceptionHandler

---
+++ src/main/java/ir/bahman/library/exception/GlobalExceptionHandler.java
@@ -0,0 +1,65 @@
+package ir.bahman.library.exception;
+
+import ir.bahman.library.dto.ExceptionResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    private ResponseEntity<ExceptionResponse> buildErrorResponse(
+            Exception ex,
+            HttpServletRequest request,
+            HttpStatus status) {
+
+        ex.printStackTrace();
+
+        ExceptionResponse response = ExceptionResponse.builder()
+                .timestamp(Instant.now().toString())
+                .status(status.value())
+                .error(status.getReasonPhrase())
+                .message(ex.getMessage())
+                .path(request.getRequestURI()).build();
+
+        return ResponseEntity.status(status).body(response);
+    }
+
+    @ExceptionHandler(AlreadyExistsException.class)
+    public ResponseEntity<ExceptionResponse> handleAlreadyExists(AlreadyExistsException ex, HttpServletRequest request) {
+        return buildErrorResponse(ex, request, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ExceptionResponse> handleEntityNotFound(EntityNotFoundException ex, HttpServletRequest request) {
+        return buildErrorResponse(ex, request, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ExceptionResponse> handleAccessDenied(AccessDeniedException ex, HttpServletRequest request) {
+        return buildErrorResponse(ex, request, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", "Validation failed");
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+
+         List<Map<String, String>> errors = ex.getBindingResult().getFieldErrors().stream()
+             .map(error -> Map.of("field", error.getField(), "message", error.getDefaultMessage()))
+             .toList();
+         body.put("errors", errors);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+}
